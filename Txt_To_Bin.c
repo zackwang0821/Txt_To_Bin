30ae33b6273222d7d597fff394878233254a8d57
Zack <wang.zack@inventec.com>
2022-10-25 14:30:45 +0800

    Implement main function.

--- Txt_To_Bin.c
+++ Txt_To_Bin.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+int main(void){
+	char name[50];
+	
+	printf("Enter the file name:");
+	scanf("&s", name);
+	//fopen(const char * filename, const char * mode) to read txt(rt) file.
+	FILE* fileIn = fopen(name, "rt");
+	
+	//If file is empty, return fail.
+	if (!fileIn){
+		fclose(fileIn);
+		return -1;
+	}
+	
+	//fopen(const char * filename, const char * mode) to write/read bin(wb+) file.
+	FILE* fileOut = fopen("output.bin", "wb+");
+	if (!fileOut){
+		fclose(fileOut);
+		return -1;
+	}
+	
+	return 0;
+	
+}
